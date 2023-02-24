a132f73fd35a279165b1a13ee4d569dea3526245
alireza talebizadeh <alireza.talebizadeh78@gmail.com>
2023-02-24 18:49:13 +0330

    edit date

--- content/js/app.js
+++ content/js/app.js
@@ -134,34 +134,6 @@ let apiData = {
   key: '26c4d8ad14b57209671494df9bd9fcb9'
 }
 
-// let imgSrc = ['/content/images/bg.jpg', '/content/images/WinterSnow.jpg', '/content/images/عکس-زمینه-آسمان-با-ابر.jpg']
-
-
-// fetch data with api
-function fetchDataFunc() {
-  fetch(`${apiData.url}${inputElem.value}&appid=${apiData.key}`)
-    .then(res => res.json())//convert type of data
-    .then(data => {
-      showData(data)//recieve data and send this to show it
-    })
-}
-
-
-//show data with recieve data
-function showData(data) {
-  // console.log(data);
-  mainElem.innerHTML = `
-<section class="location">
-    <div class="city">${data.name}, ${data.sys.country}</div>
-</section>
-<div class="current">
-    <div class="temp">${(Math.floor(data.main.temp) - 273)}<span>°c</span></div>
-    <div class="weather">${data.weather[0].main}</div>
-    <div class="hi-low">${(Math.floor(data.main.temp_min) - 273)}°c / ${(Math.floor(data.main.temp_max) - 273)}°c</div>
-</div>`
-}
-
-
 //Show user time
 function showDate() {
   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -176,9 +148,39 @@ function showDate() {
     date: timeUser.getDate(),
   }
 
-  dateElem.innerHTML = `${now.date} ${now.day} ${now.month} ${now.year}`
+  return `${now.date} ${now.day} ${now.month} ${now.year}`
 }
 
+// fetch data with api
+function fetchDataFunc() {
+  fetch(`${apiData.url}${inputElem.value}&appid=${apiData.key}`)
+    .then(res => res.json())//convert type of data
+    .then(data => {
+      showData(data)//recieve data and send this to show it
+    })
+}
+
+
+//show data with recieve data
+function showData(data) {
+
+
+  mainElem.innerHTML = `
+<section class="location">
+    <div class="city">${data.name}, ${data.sys.country}</div>
+    <div class="date">${showDate()}</div>
+</section>
+<div class="current">
+    <div class="temp">${(Math.floor(data.main.temp) - 273)}<span>°c</span></div>
+    <div class="weather">${data.weather[0].main}</div>
+    <div class="hi-low">${(Math.floor(data.main.temp_min) - 273)}°c / ${(Math.floor(data.main.temp_max) - 273)}°c</div>
+</div>`
+}
+
+
+
+
 inputElem.addEventListener('keyup', fetchDataFunc)
+inputElem.addEventListener('keyup', showDate)
 //show date with loaded windows
 window.addEventListener('load', showDate)
